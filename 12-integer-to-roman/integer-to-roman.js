e4a8ac2eeeeca7b3890cf02505f7f3fcb668475c
Prasad Bhagyawant <89301130+Prasadbh29@users.noreply.github.com>
2025-04-11 19:29:21 +0530

    Time: 4 ms (83.72%) | Memory: 59.7 MB (80.03%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,24 @@
+function intToRoman(num) {
+  const val = [
+    1000, 900, 500, 400, 
+    100, 90, 50, 40, 
+    10, 9, 5, 4, 1
+  ];
+
+  const symbol = [
+    "M", "CM", "D", "CD", 
+    "C", "XC", "L", "XL", 
+    "X", "IX", "V", "IV", "I"
+  ];
+
+  let result = '';
+
+  for (let i = 0; i < val.length; i++) {
+    while (num >= val[i]) {
+      result += symbol[i];
+      num -= val[i];
+    }
+  }
+
+  return result;
+}
